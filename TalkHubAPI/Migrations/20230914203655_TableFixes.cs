08817cfd9ac218ddbac8e2efc12047c25a9a1e74
Jordan-Dimitrov <dani27game@gmail.com>
2023-09-15 00:02:14 +0300

    added migrations and more seeding to the table

---
+++ TalkHubAPI/Migrations/20230914203655_TableFixes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TalkHubAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class TableFixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- TalkHubAPI/Seed.cs
+++ TalkHubAPI/Seed.cs
@@ -5,7 +5,9 @@ using TalkHubAPI.Data;
 using TalkHubAPI.Interfaces;
 using TalkHubAPI.Models;
 using TalkHubAPI.Models.ForumModels;
+using TalkHubAPI.Models.MessengerModels;
 using TalkHubAPI.Models.PhotosManagerModels;
+using TalkHubAPI.Models.VideoPlayerModels;
 
 namespace TalkHubAPI
 {
@@ -66,7 +68,7 @@ namespace TalkHubAPI
                 {
                     new Photo()
                     {
-                        FileName = "fuze.png",
+                        FileName = "hidden.png",
                         Timestamp = DateTime.Now,
                         Description = "CLUSTER CHARGE GOING LIVE",
                         UserId = _Context.Users.First().Id,
@@ -74,7 +76,7 @@ namespace TalkHubAPI
                     },
                     new Photo()
                     {
-                        FileName = "tachanka.png",
+                        FileName = "hidden.png",
                         Timestamp = DateTime.Now,
                         Description = "lord tachanka",
                         UserId = _Context.Users.First().Id + 1,
@@ -107,7 +109,7 @@ namespace TalkHubAPI
                     new ForumMessage()
                     {
                         MessageContent = "Starting area glitch",
-                        FileName = "link.png",
+                        FileName = "hidden.png",
                         UserId = _Context.Users.First().Id,
                         DateCreated = DateTime.Now,
                         ForumThreadId = _Context.ForumThreads.First().Id,
@@ -116,7 +118,7 @@ namespace TalkHubAPI
                     new ForumMessage()
                     {
                         MessageContent = "Good one",
-                        FileName = "jotaro.png",
+                        FileName = "hidden.png",
                         UserId = _Context.Users.First().Id + 1,
                         DateCreated = DateTime.Now,
                         ForumThreadId = _Context.ForumThreads.First().Id,
@@ -138,6 +140,185 @@ namespace TalkHubAPI
                 };
 
                 _Context.UserUpvotes.AddRange(userUpvotes);
+                _Context.SaveChanges();
+
+                List<MessageRoom> rooms = new List<MessageRoom>()
+                {
+                    new MessageRoom()
+                    {
+                        RoomName = "chill room"
+                    },
+                    new MessageRoom()
+                    {
+                        RoomName = "sad room"
+                    }
+                };
+
+                _Context.MessageRooms.AddRange(rooms);
+                _Context.SaveChanges();
+
+                List<MessengerMessage> messengerMessages = new List<MessengerMessage>()
+                {
+                    new MessengerMessage()
+                    {
+                        UserId = _Context.Users.First().Id,
+                        RoomId = _Context.MessageRooms.First().Id,
+                        MessageContent = "hi",
+                        DateCreated = DateTime.Now,
+                    },
+                    new MessengerMessage()
+                    {
+                        UserId = _Context.Users.First().Id + 1,
+                        RoomId = _Context.MessageRooms.First().Id + 1,
+                        MessageContent = "hey",
+                        DateCreated = DateTime.Now,
+                    }
+                };
+
+                _Context.MessengerMessages.AddRange(messengerMessages);
+                _Context.SaveChanges();
+
+                List<UserMessageRoom> userMessageRooms = new List<UserMessageRoom>()
+                {
+                    new UserMessageRoom()
+                    {
+                        UserId = _Context.Users.First().Id,
+                        RoomId = _Context.MessageRooms.First().Id,
+                    },
+                    new UserMessageRoom()
+                    {
+                        UserId = _Context.Users.First().Id + 1,
+                        RoomId = _Context.MessageRooms.First().Id + 1,
+                    }
+                };
+
+                _Context.UserMessageRooms.AddRange(userMessageRooms);
+                _Context.SaveChanges();
+
+                List<VideoTag> videoTags = new List<VideoTag>()
+                {
+                    new VideoTag()
+                    {
+                        TagName = "funny"
+                    },
+                    new VideoTag()
+                    {
+                        TagName = "television"
+                    }
+                };
+
+                _Context.VideoTags.AddRange(videoTags);
+                _Context.SaveChanges();
+
+                List<Video> videos = new List<Video>()
+                {
+                    new Video()
+                    {
+                        VideoName = "The story of Geralt of Rivia",
+                        Mp4name = "hidden_video.mp4",
+                        ThumbnailName = "hidden.png",
+                        VideoDescription = "The full story",
+                        LikeCount = 0,
+                        UserId = _Context.Users.First().Id,
+                        TagId = _Context.VideoTags.First().Id
+                    },
+                    new Video()
+                    {
+                        VideoName = "Death note lore",
+                        Mp4name = "hidden_video.mp4",
+                        ThumbnailName = "hidden.png",
+                        VideoDescription = "The full story",
+                        LikeCount = 0,
+                        UserId = _Context.Users.First().Id + 1,
+                        TagId = _Context.VideoTags.First().Id
+                    }
+                };
+
+                _Context.Videos.AddRange(videos);
+                _Context.SaveChanges();
+
+                List<VideoComment> videoComments = new List<VideoComment>()
+                {
+                    new VideoComment()
+                    {
+                        MessageContent = "nice video",
+                        DateCreated = DateTime.Now,
+                        LikeCount = 0,
+                        UserId = _Context.Users.First().Id + 1,
+                        VideoId = _Context.Videos.First().Id,
+                    },
+                    new VideoComment()
+                    {
+                        MessageContent = "wow",
+                        DateCreated = DateTime.Now,
+                        LikeCount = 0,
+                        ReplyId = _Context.VideoComments.First().Id,
+                        UserId = _Context.Users.First().Id,
+                        VideoId = _Context.Videos.First().Id,
+                    }
+                };
+
+                _Context.VideoComments.AddRange(videoComments);
+                _Context.SaveChanges();
+
+                List<Playlist> playlists = new List<Playlist>()
+                {
+                    new Playlist()
+                    {
+                        PlaylistName = "funny",
+                        UserId = _Context.Users.First().Id,
+                    },
+                    new Playlist()
+                    {
+                        PlaylistName = "television",
+                        UserId= _Context.Users.First().Id + 1,
+                    }
+                };
+
+                _Context.Playlists.AddRange(playlists);
+                _Context.SaveChanges();
+
+                List<VideoPlaylist> videoPlaylists = new List<VideoPlaylist>()
+                {
+                    new VideoPlaylist()
+                    {
+                        PlaylistId = _Context.Playlists.First().Id,
+                        VideoId= _Context.Videos.First().Id
+                    },
+                    new VideoPlaylist()
+                    {
+                        PlaylistId = _Context.Playlists.First().Id,
+                        VideoId= _Context.Videos.First().Id + 1
+                    }
+                };
+
+                _Context.VideoPlaylists.AddRange(videoPlaylists);
+                _Context.SaveChanges();
+
+                List<VideoCommentsLike> videoCommentsLikes = new List<VideoCommentsLike>()
+                {
+                    new VideoCommentsLike()
+                    {
+                        UserId = _Context.Users.First().Id,
+                        VideoCommentId = _Context.VideoComments.First().Id,
+                        Rating = 1,
+                    }
+                };
+
+                _Context.VideoCommentsLikes.AddRange(videoCommentsLikes);
+                _Context.SaveChanges();
+
+                List<VideoUserLike> videoUserLikes = new List<VideoUserLike>()
+                {
+                    new VideoUserLike()
+                    {
+                        UserId = _Context.Users.First().Id,
+                        VideoId = _Context.Videos.First().Id,
+                        Rating = 1,
+                    }
+                };
+
+                _Context.VideoUserLikes.AddRange(videoUserLikes);
                 _Context.SaveChanges();
             }
         }
