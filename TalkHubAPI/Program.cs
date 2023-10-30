6cb8a5b0ceeda253ae79a3ec4e1cb31d487f4731
Jordan-Dimitrov <iordan.dimitrov.highschool@svetlina.net>
2023-10-30 14:37:50 +0200

    fixed the gitignore

--- TalkHubAPI/Program.cs
+++ TalkHubAPI/Program.cs
@@ -31,7 +31,6 @@ namespace TalkHubAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllers();
             builder.Services.AddTransient<Seed>();
             builder.Services.AddControllers().AddJsonOptions(x =>
